73c66846bc918f6a19121c60fed85a9b938f25f9
Jeff Thomas <jeffry@eltoro.com>
2021-06-25 16:40:57 -0400

    testing

--- src/Requests/Methods/heroes.ts
+++ src/Requests/Methods/heroes.ts
@@ -1,6 +1,16 @@
 import requests from '../http'
 
+const baseUrl = 'https://api.pandascore.co/ow'
+
 export const getHeroes = async () => {
+  fetch(`${baseUrl}/heroes`, {
+    method: 'get',
+    headers: new Headers({
+      Authorization: `Bearer ${process.env.REACT_APP_BEARER}`,
+    })
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
   return await requests.get('/heroes')
 }
 
