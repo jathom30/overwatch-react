16975bd4acdb41d534b1fcf7e50d1239f907d934
Jeff Thomas <jeffry@eltoro.com>
2021-06-25 16:44:08 -0400

    remove unneeded fetch

--- src/Requests/Methods/heroes.ts
+++ src/Requests/Methods/heroes.ts
@@ -1,23 +1,10 @@
 import requests from '../http'
 
-const baseUrl = 'https://api.pandascore.co/ow'
+export const getHeroes = async () =>
+  await requests.get('/heroes')
 
-export const getHeroes = async () => {
-  fetch(`${baseUrl}/heroes`, {
-    method: 'get',
-    headers: new Headers({
-      Authorization: `Bearer ${process.env.REACT_APP_BEARER}`,
-    })
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-  return await requests.get('/heroes')
-}
+export const getHero = async (id: number) =>
+  await requests.get(`/heroes/${id}`)
 
-export const getHero = async (id: number) => {
-  return await requests.get(`/heroes/${id}`)
-}
-
-export const getMaps = async () => {
-  return await requests.get('/maps')
-}
+export const getMaps = async () =>
+  await requests.get('/maps')
