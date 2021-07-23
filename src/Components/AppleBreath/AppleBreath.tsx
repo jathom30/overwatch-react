54707b8f9dab1a6f4e2bc84cbd6677af03c538a5
Jeff Thomas <jeffry@eltoro.com>
2021-07-23 12:31:23 -0400

    apple breath loader

---
+++ src/Components/AppleBreath/AppleBreath.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './AppleBreath.scss'
+
+export const AppleBreath = () => {
+  return (
+    <div className="AppleBreath">
+      <div className="AppleBreath__center" />
+      <div className="AppleBreath__container">
+        {Array.from({ length: 6}, (v,i) => (
+          <div key={i} className={`AppleBreath__circle-container AppleBreath__circle-container--${i}`}>
+            <div className="AppleBreath__circle" />
+          </div>
+        ))}
+      </div>
+      <div className="AppleBreath__background" />
+    </div>
+  )
+}
--- src/Views/Home/Home.tsx
+++ src/Views/Home/Home.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { HeroesIcon, MapsIcon } from 'Assets'
-import { Loading } from 'Components'
+// import { Link } from 'react-router-dom'
+// import { HeroesIcon, MapsIcon } from 'Assets'
+import { AppleBreath, Loading } from 'Components'
 import './Home.scss'
 
 export const Home = () => {
   return (
     <div className="Home">
-      <h1 className="Home__finding-game">Finding game</h1>
+      {/* <h1 className="Home__finding-game">Finding game</h1> */}
       <Loading />
-      <span className="Home__searching">Searching for a game</span>
+      <div className="Home__apple-background">
+        <AppleBreath />
+      </div>
+      {/* <span className="Home__searching">Searching for a game</span>
       <div className="Home__quick-links">
         <Link to="/heroes"><HeroesIcon /></Link>
         <Link to="/maps"><MapsIcon /></Link>
@@ -17,7 +20,7 @@ export const Home = () => {
       <div className="Home__disclaimer">
         <p>Disclaimer: Current API used may not be 100% accurate</p>
         <a href="https://developers.pandascore.co/doc/index_ow.htm#" target="_blank" rel="noopener noreferrer">Current API docs</a>
-      </div>
+      </div> */}
     </div>
   )
 }
